dee3b698c5084112d33fd22d1089b9f3d4cfc5da
gabrielanori <gabriela.nori1@gmail.com>
2020-09-23 19:53:46 -0300

    first commit

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,10 @@
+console.log("Meu primeiro programa");
+console.log("Meu Primeiro programa: trabalhando com variáveis");
+const idade = 26;
+console.log("idade");
+const idadeSomada = idade+2;
+console.log(idadeSomada);
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
